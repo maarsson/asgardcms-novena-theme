ba51070e3ef8d09caef42c011b48a9382f606052
VMaarsson <viktor@maarsson.se>
2022-01-22 13:03:07 +0100

    Add dynamic content to header

--- views/layouts/header/top.blade.php
+++ views/layouts/header/top.blade.php
@@ -3,16 +3,44 @@
             <div class="row align-items-center">
                 <div class="col-lg-6">
                     <ul class="top-bar-info list-inline-item pl-0 mb-0">
-                        <li class="list-inline-item"><a href="mailto:support@gmail.com"><i class="icofont-support-faq mr-2"></i>support@novena.com</a></li>
-                        <li class="list-inline-item"><i class="icofont-location-pin mr-2"></i>Address Ta-134/A, New York, USA </li>
+                        @if(@setting('configuration::email-address'))
+                        <li class="list-inline-item"><a href="mailto:@setting('configuration::email-address')"><i class="icofont-support-faq mr-2"></i>@setting('configuration::email-address')</a></li>
+                        @endif
+                        @if(@setting('configuration::location'))
+                        <li class="list-inline-item"><i class="icofont-location-pin mr-2"></i>@setting('configuration::location')</li>
+                        @endif
                     </ul>
                 </div>
-                <div class="col-lg-6">
+                <div class="col-lg-2">
+                @if(count(LaravelLocalization::getSupportedLocales())>1)
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <i class="fa fa-flag"></i>
+                        <span>
+                            {{ LaravelLocalization::getCurrentLocaleName()  }}
+                            <i class="caret"></i>
+                        </span>
+                    </a>
+                    <ul class="dropdown-menu language-menu">
+                        @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
+                            <li class="{{ App::getLocale() == $localeCode ? 'active' : '' }}">
+                                <a rel="alternate" lang="{{$localeCode}}" href="{{LaravelLocalization::getLocalizedURL($localeCode) }}">
+                                    {!! $properties['native'] !!}
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </li>
+                @endif
+                </div>
+                <div class="col-lg-4">
                     <div class="text-lg-right top-right-bar mt-2 mt-lg-0">
-                        <a href="tel:+23-345-67890" >
-                            <span>Call Now : </span>
-                            <span class="h4">823-4565-13456</span>
+                        @if(@setting('configuration::phone-number'))
+                        <a href="tel:@setting('configuration::phone-number-callable')" >
+                            <span>@lang("theme::header.call-now")</span>
+                            <span class="h4">@setting('configuration::phone-number')</span>
                         </a>
+                        @endif
                     </div>
                 </div>
             </div>
