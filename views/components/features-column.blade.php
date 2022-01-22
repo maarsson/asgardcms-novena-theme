7073ecb0338b1259a1dc5f522d87729e281cdf85
VMaarsson <viktor@maarsson.se>
2022-01-22 13:05:55 +0100

    Add dynamic content to Features section

---
+++ views/components/features-column.blade.php
@@ -0,0 +1,6 @@
+                    <div class="feature-item mb-5 mb-lg-0">
+                        <div class="feature-icon mb-4">
+                            <i class="{{ $extrafields->get('column-' . $i . '-icon-class') }}"></i>
+                        </div>
+                        {!! $extrafields->get('column-' . $i) !!}
+                    </div>
--- views/sections/features.blade.php
+++ views/sections/features.blade.php
@@ -1,39 +1,16 @@
+{{-- Template: Features --}}
+@php
+$extrafields = Extrafield::getAll('features');
+@endphp
+
 <section class="features">
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
                 <div class="feature-block d-lg-flex">
-                    <div class="feature-item mb-5 mb-lg-0">
-                        <div class="feature-icon mb-4">
-                            <i class="icofont-surgeon-alt"></i>
-                        </div>
-                        <span>24 Hours Service</span>
-                        <h4 class="mb-3">Online Appoinment</h4>
-                        <p class="mb-4">Get ALl time support for emergency.We have introduced the principle of family medicine.</p>
-                        <a href="appoinment.html" class="btn btn-main btn-round-full">Make a appoinment</a>
-                    </div>
-                
-                    <div class="feature-item mb-5 mb-lg-0">
-                        <div class="feature-icon mb-4">
-                            <i class="icofont-ui-clock"></i>
-                        </div>
-                        <span>Timing schedule</span>
-                        <h4 class="mb-3">Working Hours</h4>
-                        <ul class="w-hours list-unstyled">
-                            <li class="d-flex justify-content-between">Sun - Wed : <span>8:00 - 17:00</span></li>
-                            <li class="d-flex justify-content-between">Thu - Fri : <span>9:00 - 17:00</span></li>
-                            <li class="d-flex justify-content-between">Sat - sun : <span>10:00 - 17:00</span></li>
-                        </ul>
-                    </div>
-                
-                    <div class="feature-item mb-5 mb-lg-0">
-                        <div class="feature-icon mb-4">
-                            <i class="icofont-support"></i>
-                        </div>
-                        <span>Emegency Cases</span>
-                        <h4 class="mb-3">1-800-700-6200</h4>
-                        <p>Get ALl time support for emergency.We have introduced the principle of family medicine.Get Conneted with us for any urgency .</p>
-                    </div>
+                @for ($i = 1; $i <= 3; $i++)
+                    @include('components.features-column', ['i' => $i])
+                @endfor
                 </div>
             </div>
         </div>
