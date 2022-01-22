5487807bac59b656313924f4133fc032fdb41bdb
VMaarsson <viktor@maarsson.se>
2022-01-22 12:55:56 +0100

    Apply required modules by soruce

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
+require('jquery');
+require('popper.js');
+require('bootstrap');
+require('jquery.counterup');
+require('jquery.easing');
+require('waypoints/lib/jquery.waypoints.js');
+require('waypoints/lib/shortcuts/sticky.js');
+require('shufflejs');
+require('slick-carousel');
+
 require('./bootstrap');
