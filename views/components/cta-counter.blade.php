2ef4994aad3c643e05eec38aa2513e2b1986cff2
VMaarsson <viktor@maarsson.se>
2022-01-22 13:05:13 +0100

    Refactor CTA section

---
+++ views/components/cta-counter.blade.php
@@ -0,0 +1,7 @@
+<div class="col-lg-3 col-md-6 col-sm-6">
+    <div class="counter-stat">
+        <i class="{{ $extrafields->get('counter-' . $i . '-icon-class') }}"></i>
+        <span class="h3">{{ $extrafields->get('counter-' . $i . '-value') }}</span>
+        <p>{{ $extrafields->get('counter-' . $i . '-name') }}</p>
+    </div>
+</div>
--- views/sections/cta.blade.php
+++ views/sections/cta.blade.php
@@ -7,35 +7,9 @@ $extrafields = Extrafield::getAll('cta');
     <div class="container">
         <div class="cta position-relative">
             <div class="row">
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="counter-stat">
-                        <i class="{{ $extrafields->{'counter-1-icon-class'}->value }}"></i>
-                        <span class="h3">{{ $extrafields->{'counter-1-value'}->value }}</span>
-                        <p>{{ $extrafields->{'counter-1-name'}->value }}</p>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="counter-stat">
-                        <i class="{{ $extrafields->{'counter-2-icon-class'}->value }}"></i>
-                        <span class="h3">{{ $extrafields->{'counter-2-value'}->value }}</span>
-                        <p>{{ $extrafields->{'counter-2-name'}->value }}</p>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="counter-stat">
-                        <i class="{{ $extrafields->{'counter-3-icon-class'}->value }}"></i>
-                        <span class="h3">{{ $extrafields->{'counter-3-value'}->value }}</span>
-                        <p>{{ $extrafields->{'counter-3-name'}->value }}</p>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="counter-stat">
-                        <i class="{{ $extrafields->{'counter-4-icon-class'}->value }}"></i>
-                        <span class="h3">{{ $extrafields->{'counter-4-value'}->value }}</span>
-                        <p>{{ $extrafields->{'counter-4-name'}->value }}</p>
-                    </div>
-                </div>
+            @for ($i = 1; $i <= 4; $i++)
+                @include('components.cta-counter', ['i' => $i])
+            @endfor
             </div>
         </div>
     </div>
