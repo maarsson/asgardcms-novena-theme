a780364b4cc625c82f2b5fff8320b26c5783975b
VMaarsson <viktor@maarsson.se>
2022-01-22 12:54:10 +0100

    Prepare for theme development

--- views/layouts/master.blade.php
+++ views/layouts/master.blade.php
@@ -3,8 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <title></title>
+    <link rel="stylesheet" href="{{ mix('css/app.css',  theme_assets_path()) }}" type="text/css">
 </head>
 <body>
     @yield('content')
 </body>
+<script src="{{ mix('js/app.js', theme_assets_path()) }}"></script>
 </html>
---
+++ webpack.mix.js
@@ -0,0 +1,61 @@
+const mix = require('laravel-mix');
+const WebpackShellPluginNext = require('webpack-shell-plugin-next');
+const themeInfo = require('./theme.json');
+
+
+// Set up paths
+const path = require('path');
+const appRoot = path.resolve(__dirname, '../../');
+const dotEnvFile = appRoot + '/.env';
+const themePublicPath = appRoot + '/public/themes/' + themeInfo.name.toLowerCase()
+
+// Load the .env file variables to the `process.env`
+require('mix-env-file');
+mix.env(dotEnvFile)
+
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix
+    .webpackConfig({
+       devtool: !mix.inProduction() ? 'source-map' : false
+    })
+    .setPublicPath('assets')
+    .js('resources/js/app.js', 'assets/js/app.js')
+    .sass('resources/scss/app.scss', 'assets/css/app.css')
+    .sourceMaps(!mix.inProduction())
+    // Live reload on watch
+    .browserSync({
+        proxy: process.env.APP_URL,
+        files: [
+            'app/**/*.php',
+            'resources/views/**/*.php',
+            themePublicPath + '/js/**/*.js',
+            themePublicPath + '/css/**/*.css',
+        ]
+    })
+    // Publishing theme assets
+    .webpackConfig({
+        plugins: [
+            new WebpackShellPluginNext({
+                onAfterDone: {
+                    scripts: ['php ../../artisan stylist:publish ' + themeInfo.name],
+                    blocking: true,
+                    parallel: false
+                }
+            })
+        ]
+    });
+
+if (mix.inProduction()) {
+    mix.version();
+}
