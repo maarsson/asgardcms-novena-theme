436b32f4974433747c8403003e303a6dd628358c
VMaarsson <viktor@maarsson.se>
2022-01-22 13:07:07 +0100

    Add extrafield module configuration

---
+++ config/extrafields.php
@@ -0,0 +1,70 @@
+<?php
+
+return [
+    'name' => 'Extrafield',
+    'block_template_fields' => [
+        'about' => [
+            'translatable' => [
+            ],
+            'normal' => [
+                'image-1' => 'media-single',
+                'image-2' => 'media-single',
+                'image-3' => 'media-single',
+            ],
+        ],
+        'cta' => [
+            'translatable' => [
+                'counter-1-name' => 'text',
+                'counter-2-name' => 'text',
+                'counter-3-name' => 'text',
+                'counter-4-name' => 'text',
+            ],
+            'normal' => [
+                'counter-1-value' => 'text',
+                'counter-2-value' => 'text',
+                'counter-3-value' => 'text',
+                'counter-4-value' => 'text',
+                'counter-1-icon-class' => 'text',
+                'counter-2-icon-class' => 'text',
+                'counter-3-icon-class' => 'text',
+                'counter-4-icon-class' => 'text',
+            ],
+        ],
+        'features' => [
+            'translatable' => [
+                'column-1' => 'textarea',
+                'column-2' => 'textarea',
+                'column-3' => 'textarea',
+            ],
+            'normal' => [
+                'column-1-icon-class' => 'text',
+                'column-2-icon-class' => 'text',
+                'column-3-icon-class' => 'text',
+            ],
+        ],
+        'services' => [
+            'translatable' => [
+                'box-1-title' => 'text',
+                'box-1-text' => 'text',
+                'box-2-title' => 'text',
+                'box-2-text' => 'text',
+                'box-3-title' => 'text',
+                'box-3-text' => 'text',
+                'box-4-title' => 'text',
+                'box-4-text' => 'text',
+                'box-5-title' => 'text',
+                'box-5-text' => 'text',
+                'box-6-title' => 'text',
+                'box-6-text' => 'text',
+            ],
+            'normal' => [
+                'box-1-icon-class' => 'text',
+                'box-2-icon-class' => 'text',
+                'box-3-icon-class' => 'text',
+                'box-4-icon-class' => 'text',
+                'box-5-icon-class' => 'text',
+                'box-6-icon-class' => 'text',
+            ],
+        ],
+    ],
+];
