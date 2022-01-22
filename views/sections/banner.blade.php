d410fc273a2255d48fee00717dc7cd720aac8365
VMaarsson <viktor@maarsson.se>
2022-01-22 13:07:41 +0100

    Invert text color of head banner

--- views/sections/banner.blade.php
+++ views/sections/banner.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-6 col-md-12 col-xl-7">
-                <div class="block">
+                <div class="block inverted">
                     <div class="divider mb-3"></div>
                     <span class="text-uppercase text-sm letter-spacing ">Total Health care solution</span>
                     <h1 class="mb-3 mt-3">Your most trusted health partner</h1>
