f305febaf82c923e4ed704323c1e096a0b967d2c
VMaarsson <viktor@maarsson.se>
2022-01-22 13:04:56 +0100

    Add dynamic content to CTA section

--- views/sections/cta.blade.php
+++ views/sections/cta.blade.php
@@ -1,34 +1,39 @@
+{{-- Template: CTA section --}}
+@php
+$extrafields = Extrafield::getAll('cta');
+@endphp
+
 <section class="cta-section ">
     <div class="container">
         <div class="cta position-relative">
             <div class="row">
                 <div class="col-lg-3 col-md-6 col-sm-6">
                     <div class="counter-stat">
-                        <i class="icofont-doctor"></i>
-                        <span class="h3">58</span>k
-                        <p>Happy People</p>
+                        <i class="{{ $extrafields->{'counter-1-icon-class'}->value }}"></i>
+                        <span class="h3">{{ $extrafields->{'counter-1-value'}->value }}</span>
+                        <p>{{ $extrafields->{'counter-1-name'}->value }}</p>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-6">
                     <div class="counter-stat">
-                        <i class="icofont-flag"></i>
-                        <span class="h3">700</span>+
-                        <p>Surgery Comepleted</p>
+                        <i class="{{ $extrafields->{'counter-2-icon-class'}->value }}"></i>
+                        <span class="h3">{{ $extrafields->{'counter-2-value'}->value }}</span>
+                        <p>{{ $extrafields->{'counter-2-name'}->value }}</p>
                     </div>
                 </div>
-                
+
                 <div class="col-lg-3 col-md-6 col-sm-6">
                     <div class="counter-stat">
-                        <i class="icofont-badge"></i>
-                        <span class="h3">40</span>+
-                        <p>Expert Doctors</p>
+                        <i class="{{ $extrafields->{'counter-3-icon-class'}->value }}"></i>
+                        <span class="h3">{{ $extrafields->{'counter-3-value'}->value }}</span>
+                        <p>{{ $extrafields->{'counter-3-name'}->value }}</p>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-6">
                     <div class="counter-stat">
-                        <i class="icofont-globe"></i>
-                        <span class="h3">20</span>
-                        <p>Worldwide Branch</p>
+                        <i class="{{ $extrafields->{'counter-4-icon-class'}->value }}"></i>
+                        <span class="h3">{{ $extrafields->{'counter-4-value'}->value }}</span>
+                        <p>{{ $extrafields->{'counter-4-name'}->value }}</p>
                     </div>
                 </div>
             </div>
